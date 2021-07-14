2bbe98aeec3e2318e226e7c227b91b7d7b806f5d
Skyler vautier <skyler8@icloud.com>
2021-07-14 07:12:25 -0400

    fml

--- api/src/app.ts
+++ api/src/app.ts
@@ -16,8 +16,8 @@ import { KittyItemsService } from "./services/kitty-items";
 import { MarketService } from "./services/market";
 
 //WebSocketServer
-//import WebSocket from "ws";
-//import http from "http";
+import WebSocket from "ws";
+import http from "http";
 
 
 const V1 = "/v1/";
@@ -37,7 +37,7 @@ const initApp = (
   app.use(V1, initKibblesRouter(kibblesService));
   app.use(V1, initKittyItemsRouter(kittyItemsService));
   app.use(V1, initMarketRouter(marketService));
-
+/*
   const serveReactApp = () => {
     app.use(express.static(path.resolve(__dirname, "../../web/public")));
 
@@ -48,13 +48,13 @@ const initApp = (
       res.sendFile(path.resolve(__dirname, "../../web/public/index.html"));
     });
   };
-
+*/
   //Test WebSocketServer
-/*
-  var app2 = express()
+
+  //var app2 = express()
   app.use(express.static(path.resolve(__dirname, "../../web/build")))
   var port = process.env.PORT || 5000
-  var server = http.createServer(app2)
+  var server = http.createServer(app)
   server.listen(port)
 
   console.log("http server listening on %d", port)
@@ -74,14 +74,14 @@ const initApp = (
   })
 })
 
-*/
 
 
+/*
   if (process.env.IS_HEROKU) {
     // Serve React static site using Express when deployed to Heroku.
     serveReactApp();
   }
-
+*/
   app.all("*", async (req: Request, res: Response) => {
     return res.sendStatus(404);
   });
