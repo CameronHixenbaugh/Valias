254f7d3ce270de7ac5ddad624bedb78a8dfc0046
Skyler vautier <skyler8@icloud.com>
2021-11-10 14:59:34 -0500

    errors

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,6 +2,6 @@
 
 # Build the apps, don't attempt to build fcl-dev-wallet. 
 
-tsnd install --ignore=fcl-dev-wallet 
-npm run build --ignore=fcl-dev-wallet
+npx lerna exec npm install --ignore=fcl-dev-wallet 
+npx lerna exec npm run build --ignore=fcl-dev-wallet 
  
